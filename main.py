9c1e433b79224319b8ec2bb5e8f50631ab24231b
ricardo corcho <carranzar@uninorte.edu.co>
2019-10-28 16:10:57 -0600

    agrege id a decorator de getmessages

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import hug
 import modelo_bayesiano
 
 
-@hug.get("/getmessages/{id}")
-def messages(id: hug.types.text):
-    return modelo_bayesiano.getreputation(id)
+@hug.get("/"/"")
+def messages():
+    return "hola"
